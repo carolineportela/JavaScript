2d0ca267895516b9df61beea63dcf323498324d3
carolPortela <carol10.portela12@gmail.com>
2023-02-09 22:01:07 -0300

    Tabuada com entrada de dados feita

--- Aula 04/app.js
+++ Aula 04/app.js
@@ -10,24 +10,46 @@ var entradaDados = readline.createInterface({
     output: process.stdout
 });
 
-//valor 1
-entradaDados.question('Digite o mínimo multiplicador\n',function (multiplicando){
+
+entradaDados.question('Digite o mínimo multiplicador\n', function (multiplicando) {
     let numero1 = multiplicando;
 
-    entradaDados.question('Digite o máximo multiplicador\n',function (maxMultiplicador){
-     let numero2 = maxMultiplicador;
+    entradaDados.question('Digite o máximo multiplicador\n', function (maxMultiplicador) {
+        let numero2 = maxMultiplicador;
 
-     let calcularTabuada = 
+        if (numero1 == '' || numero2 == '') {
+            console.log('ERRO: Não é possivel calcular sem que tenha sido digitado um valor');
+        }else if(numero2 == 0){
+            console.log('ERRO: O Máximo multiplicador não pode ser 0');
+
+        } 
+        else if (isNaN(numero1) || isNaN(numero2)) {
+            console.log('Você digitou letras, dgite números')
+        }
+
+       
+        else {
+            //resultado recebe calcularTabuada,se o resultado for diferente
+            // ou igual a true retorna o resultado no console.
+            resultado = matematica.calcularTabuada(multiplicando, maxMultiplicador)
+            if (resultado !== true) {
+                console.log(resultado);
+            } else {
+                //(encerra o programa)
+                entradaDados.close();
+            };
+
+        }
 
     });
-    
+
 });
 
 
-   
 
-    
-    
+
+
+
 
 
 
